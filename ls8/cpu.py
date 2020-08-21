81d8b08fade0e7e3bb71d8bb1e8f8a9ce11e9769
John <cjd8801@gmail.com>
2020-08-21 11:00:18 -0500

    fixing bugs

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,12 +1,8 @@
-"""CPU functionality."""
-
 import sys
 
 class CPU:
-    """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
@@ -15,15 +11,23 @@ class CPU:
         # Set the stack pointer to R7
         self.reg[7] = 0xF4
 
+        # Set the flag register
+        self.flag = 0b00000000
+
         self.branch_table = {
             0b10000010: self.LDI,
             0b01000111: self.PRN,
             0b00000001: self.HLT,
             0b10100010: self.MUL,
+            0b10100000: self.ADD,
             0b01000101: self.PUSH,
             0b01000110: self.POP,
             0b01010000: self.CALL,
-            0b00010001: self.RET
+            0b00010001: self.RET,
+            0b10100111: self.CMP,
+            0b01010100: self.JMP,
+            0b01010101: self.JEQ,
+            0b01010110: self.JNE
         }
 
     # Should accept the address to read and return the value
@@ -87,39 +91,61 @@ class CPU:
         # Update program counter
         self.pc = address
 
-    def load(self):
-        fileName = sys.argv[1]
-        """Load a program into memory."""
+    def CMP(self):
+        reg_a = self.ram[self.pc + 1]
+        reg_b = self.ram[self.pc + 2]
+        self.alu('CMP', reg_a, reg_b)
+
+    def JMP(self):
+        register_index = self.ram[self.pc + 1]
+        address = self.reg[register_index]
+        self.pc = address
+
+    def JEQ(self):
+        register_index = self.ram[self.pc + 1]
+        if self.flag == 1:
+            self.pc = self.reg[register_index] # Set program counter to value at given register
+        else:
+            self.pc += 2
+
+    def JNE(self):
+        register_index = self.ram[self.pc + 1]
+        if self.flag == 0:
+            self.pc = self.reg[register_index] # Set program counter to value at given register
+        else:
+            self.pc += 2
+
+    def load(self, fileName):
         address = 0
         with open(fileName) as file:
             for line in file:
                 line = line.split('#')
                 try:
                     instruction = int(line[0], 2)
+                    self.ram[address] = instruction
+                    address += 1
                 except ValueError:
                     continue
-                self.ram_write(address, instruction)
-                address += 1
                     
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
             self.pc += 3
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
             self.pc += 3
+        elif op == "CMP":
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.flag = 1 #0b00000001
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
+            #self.fl,
+            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -131,7 +157,6 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
         while self.running:
             ir = self.ram[self.pc]
             try:
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-
-"""Main."""
-
 import sys
-from cpu import*
+from cpu import *
 
 cpu = CPU()
 
-# try:
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
+
