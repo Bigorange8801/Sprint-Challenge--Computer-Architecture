f7d8ada61c01dfa2523846122a40bcbc9e57a897
John <cjd8801@gmail.com>
2020-08-21 10:14:08 -0500

    added files to sprint

---
+++ ls8/cpu.py
@@ -0,0 +1,141 @@
+"""CPU functionality."""
+
+import sys
+
+class CPU:
+    """Main CPU class."""
+
+    def __init__(self):
+        """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = True
+
+        # Set the stack pointer to R7
+        self.reg[7] = 0xF4
+
+        self.branch_table = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET
+        }
+
+    # Should accept the address to read and return the value
+    def ram_read(self, address):
+        return self.ram[address]
+
+    # Should accept an address and value and store the value at that address
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
+    def LDI(self):
+        index = self.ram[self.pc + 1]
+        value = self.ram[self.pc + 2]
+        self.reg[index] = value
+        self.pc += 3
+
+    def PRN(self):
+        value = self.reg[self.ram[self.pc + 1]]
+        print(value)
+        self.pc += 2
+
+    def HLT(self):
+        self.running = False
+
+    def MUL(self):
+        reg_a = self.ram[self.pc + 1]
+        reg_b = self.ram[self.pc + 2]
+        self.alu('MUL', reg_a, reg_b)
+
+    def ADD(self):
+        reg_a = self.ram[self.pc + 1]
+        reg_b = self.ram[self.pc + 2]
+        self.alu('ADD', reg_a, reg_b)
+
+    def PUSH(self):
+        self.reg[7] -= 1
+        register_index = self.ram[self.pc + 1]
+        self.ram[self.reg[7]] = self.reg[register_index]
+        self.pc += 2
+
+    def POP(self):
+        register_index = self.ram[self.pc + 1]
+        self.reg[register_index] = self.ram[self.reg[7]]
+        self.reg[7] += 1
+        self.pc += 2
+
+    def CALL(self):
+        # Store next instruction in the stack
+        self.reg[7] -= 1
+        self.ram[self.reg[7]] = self.pc + 2
+        # Move program counter to address stored in register
+        register_index = self.ram[self.pc + 1]
+        self.pc = self.reg[register_index]
+
+    def RET(self):
+        # Pop from the stack
+        address = self.ram[self.reg[7]]
+        register_index = self.ram[self.pc + 1]
+        self.reg[register_index] = self.ram[self.reg[7]]
+        self.reg[7] += 1
+        # Update program counter
+        self.pc = address
+
+    def load(self):
+        fileName = sys.argv[1]
+        """Load a program into memory."""
+        address = 0
+        with open(fileName) as file:
+            for line in file:
+                line = line.split('#')
+                try:
+                    instruction = int(line[0], 2)
+                except ValueError:
+                    continue
+                self.ram_write(address, instruction)
+                address += 1
+                    
+    def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+            self.pc += 3
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+            self.pc += 3
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
+
+    def run(self):
+        """Run the CPU."""
+        while self.running:
+            ir = self.ram[self.pc]
+            try:
+                self.branch_table[ir]()
+            except:
+                raise Exception(f"Unknown instruction: {self.ram[self.pc]}")
+
---
+++ ls8/ls8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import*
+
+cpu = CPU()
+
+# try:
+cpu.load()
+cpu.run()
